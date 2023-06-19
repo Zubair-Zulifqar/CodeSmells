7e99668d60681d36865aab5b6519a228c9b1b23b
Zubair Zulifqar <91710951+Zubair-Zulifqar@users.noreply.github.com>
2023-06-20 01:22:18 +0500

    Structure Redefined

---
+++ app/src/main/java/com/example/applicationtomaintainprogressofhafizstudent/DeleteStudent.java
@@ -0,0 +1,14 @@
+package com.example.applicationtomaintainprogressofhafizstudent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DeleteStudent extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_delete_student);
+    }
+}
