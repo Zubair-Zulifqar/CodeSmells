6dcd6384099617c294d8f578cfcd41f706f46ff4
Zubair Zulifqar <91710951+Zubair-Zulifqar@users.noreply.github.com>
2023-06-20 05:30:23 +0500

    Recyclar View Error Removing

--- app/src/main/java/com/example/applicationtomaintainprogressofhafizstudent/SearchStudent.java
+++ app/src/main/java/com/example/applicationtomaintainprogressofhafizstudent/SearchStudent.java
@@ -1,15 +1,22 @@
 package com.example.applicationtomaintainprogressofhafizstudent;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class SearchStudent extends AppCompatActivity {
+    List<Student> StudentsList = new ArrayList<>();
+    RecyclerView recyclerView;
+    RecyclerView.Adapter adapter;
+    RecyclerView.LayoutManager layoutManager;
     EditText etName;
     List<Student> listofstudents;
     Button btnSearch;
@@ -36,14 +43,27 @@ public class SearchStudent extends AppCompatActivity {
                 @Override
                 public void onClick(View view) {
                     String name = etName.getText().toString();
-                    listofstudents=db.searchStudent(name);
+                    listofstudents = db.searchStudent(name);
+                    recyclerView = findViewById(R.id.recylerViewStudent);
+
+
+                    recyclerView.setHasFixedSize(true);
+
+                    //LinearLayoutManager GridLayoutManager
+                    layoutManager = new LinearLayoutManager(SearchStudent.this);
+//        layoutManager = new LinearLayoutManager(MainActivity.this,
+//                LinearLayoutManager.HORIZONTAL,
+//                false);
+                    recyclerView.setLayoutManager(layoutManager);
+
+                    adapter = new myRecyclerViewAdapter(listofstudents);
+                    recyclerView.setAdapter(adapter);
+                    //adapter.notifyDataSetChanged();
+
 
                 }
-            });
-
-        }
-
-
-
+                });
     }
-    
+}
+
+
